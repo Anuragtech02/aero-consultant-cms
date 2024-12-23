ab9075ba3660ef09dae1d565c7bdbc76e00f1dde
Anurag Pal <apal895@gmail.com>
2024-12-23 16:47:32 +0530

    added s3 support to upload

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,12 +1,46 @@
 export default [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
+  "strapi::logger",
+  "strapi::errors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:", "http"],
+          "img-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "https://hmweb-dev-bucket.s3.us-west-2.amazonaws.com",
+          ],
+          "media-src": [
+            "'self'",
+            "data:",
+            "blob:",
+            "market-assets.strapi.io",
+            "https://hmweb-dev-bucket.s3.us-west-2.amazonaws.com",
+          ],
+          upgradeInsecureRequests: null,
+        },
+      },
+      cors: {
+        origin: [
+          "http://localhost:3000",
+          "http://localhost:1337",
+          "htttp://localhost:5173",
+          "http://157.20.215.12",
+          "http://103.127.158.64",
+        ],
+      },
+    },
+  },
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
 ];
