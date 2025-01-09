c0a8cbfc820f7d8072deeeb694e843a0c124ef5e
Anurag Pal <apal895@gmail.com>
2025-01-09 20:17:26 +0530

    remove credentials

--- config/plugins.ts
+++ config/plugins.ts
@@ -9,10 +9,10 @@ export default ({ env }) => ({
         baseUrl: env("CDN_URL"),
         rootPath: env("CDN_ROOT_PATH"),
         s3Options: {
-          credentials: {
-            accessKeyId: env("AWS_ACCESS_KEY_ID"),
-            secretAccessKey: env("AWS_ACCESS_SECRET"),
-          },
+          // credentials: {
+          //   accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          //   secretAccessKey: env("AWS_ACCESS_SECRET"),
+          // },
           region: env("AWS_REGION"),
           params: {
             ACL: env("AWS_ACL", "public-read"),
